0e93fac89718df4c652642a10ba517dd674a47a9
spicyshibe <rafierasyw@gmail.com>
2025-06-12 18:23:51 +0700

    jika elemen di hapus berada di posisi terakhir array, kembali ke awal

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -65,6 +65,12 @@ class Queues
                 REAR = -1;
             }
             else
-
+            {
+                // Jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+                if (FRONT == max - 1)
+                    FRONT = 0;
+                else
+                    FRONT = FRONT + 1;
+            }
     }
 }
