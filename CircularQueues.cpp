a41124985826c5385478473c9b79c850302d4320
spicyshibe <rafierasyw@gmail.com>
2025-06-12 18:52:02 +0700

    int main done

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -128,6 +128,54 @@ int main()
 
     while (true)
     {
-        
+        try
+        {
+            cout << "menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display Value" << endl;
+            cout << "4. Exit" << endl;
+            cout << "enter your choice (1-4)" << endl;
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+                case '1':
+                {
+                    q.insert();
+                    break;
+                }
+
+                case '2':
+                {
+                    q.remove();
+                    break;
+                }
+
+                case '3':
+                {
+                    q.display();
+                    break;
+                }
+
+                case '4':
+                {
+                    return 0;
+                }
+
+                default:
+                {
+                    cout << "Invalid option!!" << endl;
+                    break;
+                }
+            }
+        }
+        catch (exception &e)
+            {
+                cout << "Check for the values entered." << endl;
+            }
     }
+    return 0;
+    
 }
